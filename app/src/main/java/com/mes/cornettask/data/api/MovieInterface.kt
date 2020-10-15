442927ebeaefb7ff068f80b88f5b5fb157ac7d9b
mes <mes@dineoutbahrain.com>
2020-10-15 23:03:09 +0200

    create data source for movies list from network

--- app/src/main/java/com/mes/cornettask/data/api/MovieInterface.kt
+++ app/src/main/java/com/mes/cornettask/data/api/MovieInterface.kt
@@ -1,12 +1,12 @@
 package com.mes.cornettask.data.api
 
 import com.mes.cornettask.data.pojos.DiscoverMoviesResponse
+import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.QueryMap
 
 interface MovieInterface {
     @GET("discover/movie")
-    fun getDiscoverMovieAsync(@QueryMap hashMap: HashMap<String, String> = HashMap()): DiscoverMoviesResponse
+    fun getDiscoverMovieAsync(): Single<DiscoverMoviesResponse>
 
     //    search/movie
 
---
+++ app/src/main/java/com/mes/cornettask/data/repositories/MoviesListDataSource.kt
@@ -0,0 +1,55 @@
+package com.mes.cornettask.data.repositories
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mes.cornettask.data.api.MovieInterface
+import com.mes.cornettask.data.pojos.DiscoverMoviesResponse
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * this class for getting list of movies from internet
+ * */
+class MoviesListDataSource
+    (
+    private val apiService: MovieInterface,
+    private val compositeDisposable: CompositeDisposable
+) {
+
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState: LiveData<NetworkState>
+        get() = _networkState                   //with this get, no need to implement get function to get networkSate
+
+    private val _moviesListResponse = MutableLiveData<DiscoverMoviesResponse>()
+    val moviesListResponse: LiveData<DiscoverMoviesResponse>
+        get() = _moviesListResponse
+
+    fun fetchMovieDetails(movieId: Int) {
+        _networkState.postValue(NetworkState.LOADING)
+        try {
+            compositeDisposable.add(
+                apiService.getDiscoverMovieAsync()
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            _moviesListResponse.postValue(it)
+                            _networkState.postValue(NetworkState.LOADED)
+                        },
+                        {
+                            _networkState.postValue(NetworkState.ERROR)
+                            it.message?.let { it1 -> Log.e("MovieDetailsDataSource", it1) }
+                        }
+                    )
+            )
+
+        } catch (e: Exception) {
+            // ?.let {} that's make sure that block of code won't be execute it e.message is null
+            e.message?.let { Log.e("MovieDetailsDataSource", it) }
+        }
+
+
+    }
+
+
+}
