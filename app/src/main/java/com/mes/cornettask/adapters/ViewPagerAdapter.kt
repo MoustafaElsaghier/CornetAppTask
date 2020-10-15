06ded7dc5feeb713d456a42e63e367fa007a885c
mes <mes@dineoutbahrain.com>
2020-10-15 21:29:41 +0200

    create pager adapter

---
+++ app/src/main/java/com/mes/cornettask/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.mes.cornettask.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.mes.cornettask.fragments.DiscoverFragment
+import com.mes.cornettask.fragments.SearchFragment
+
+class ViewPagerAdapter(supportFragmentManager: FragmentManager) :
+    FragmentStatePagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    var discoverFragment: DiscoverFragment = DiscoverFragment()
+    var searchFragment: SearchFragment = SearchFragment()
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                DiscoverFragment()
+            }
+            1 -> {
+                SearchFragment()
+            }
+
+            else -> getItem(position)
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
