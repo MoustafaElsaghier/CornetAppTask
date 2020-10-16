30fc70126688a05200e548997dd2985d695afe99
mes <mes@dineoutbahrain.com>
2020-10-16 21:50:03 +0200

    create words adapter

---
+++ app/src/main/java/com/mes/cornettask/ui/searchScreen/WordViewModel.kt
@@ -0,0 +1,31 @@
+package com.mes.cornettask.ui.searchScreen
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.mes.cornettask.data.repositories.WordRepository
+import com.mes.cornettask.database.Word
+import com.mes.cornettask.database.WordRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+
+    private val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+}
