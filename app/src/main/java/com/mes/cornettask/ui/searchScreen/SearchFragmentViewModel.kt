19b6569274d4662e9310cb8c1e048ab8d3fda43c
mes <mes@dineoutbahrain.com>
2020-10-16 15:13:11 +0200

    create search viewModel

---
+++ app/src/main/java/com/mes/cornettask/ui/searchScreen/SearchFragmentViewModel.kt
@@ -0,0 +1,35 @@
+package com.mes.cornettask.ui.searchScreen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import com.mes.cornettask.data.pojos.MovieModel
+import com.mes.cornettask.data.repositories.NetworkState
+import io.reactivex.disposables.CompositeDisposable
+
+class SearchFragmentViewModel(
+    private val moviesRepository: MoviesPageListRepository,
+    private val searchKey: String
+) :
+    ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    private val moviesPagedList: LiveData<PagedList<MovieModel>> by lazy {
+        moviesRepository.fetchLiveMoviePagedList(compositeDisposable, searchKey)
+    }
+
+    val networkState: LiveData<NetworkState> by lazy {
+        moviesRepository.getNetworkState()
+    }
+
+    fun listIsEmpty(): Boolean {
+        return moviesPagedList.value?.isEmpty() ?: true
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+
+}
