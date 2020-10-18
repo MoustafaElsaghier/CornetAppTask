f0d16b8ba95c31208f30a56716f5f60c8913ff40
mes <mes@dineoutbahrain.com>
2020-10-18 13:46:34 +0200

    add api calling & endless pagination

---
+++ app/src/main/java/com/mes/cornettask/adapters/SearchMoverAdapter.kt
@@ -0,0 +1,40 @@
+package com.mes.cornettask.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mes.cornettask.R
+import com.mes.cornettask.data.pojos.MovieModel
+import com.mes.cornettask.data.utils.ImageHelper.loadImage
+import kotlinx.android.synthetic.main.movie_item.view.*
+
+class SearchMoverAdapter(private val context: Context, private val items: List<MovieModel>) :
+    RecyclerView.Adapter<SearchMoverAdapter.DiscoverViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiscoverViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        return DiscoverViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DiscoverViewHolder, position: Int) {
+        holder.bindView(items[position], context)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class DiscoverViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        // here comes all the views / variables from view.findViewById()
+        fun bindView(item: MovieModel, context: Context) {
+            itemView.movieName.text = item.title
+            itemView.movieReleaseDate.text = item.releaseDate
+            itemView.movieOverview.text = item.overview
+
+            loadImage(context, itemView.movieImage, item.posterPath)
+
+        }
+    }
+
+}
--- app/src/main/java/com/mes/cornettask/ui/normalsearch/NormalSearchFragment.kt
+++ app/src/main/java/com/mes/cornettask/ui/normalsearch/NormalSearchFragment.kt
@@ -3,22 +3,35 @@ package com.mes.cornettask.ui.normalsearch
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mes.cornettask.R
-import com.mes.cornettask.adapters.PopularMoviePagedListAdapter
+import com.mes.cornettask.adapters.SearchMoverAdapter
 import com.mes.cornettask.adapters.WordListAdapter
+import com.mes.cornettask.data.api.ApiClient
+import com.mes.cornettask.data.api.MovieInterface
+import com.mes.cornettask.data.pojos.MovieModel
+import com.mes.cornettask.data.utils.EndlessRecyclerViewScrollListener
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class NormalSearchFragment : Fragment() {
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private val moviesList: ArrayList<MovieModel> = ArrayList()
+    private lateinit var apiService: MovieInterface
     private lateinit var wordsAdapter: WordListAdapter
     private var searchKey: String = ""
-    private lateinit var moviesAdapter: PopularMoviePagedListAdapter
+    private lateinit var moviesAdapter: SearchMoverAdapter
+    private val compositeDisposable = CompositeDisposable()
+    lateinit var scroller: EndlessRecyclerViewScrollListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,23 +48,39 @@ class NormalSearchFragment : Fragment() {
     }
 
     private fun initViews() {
-//        wordViewModel = initWordsViewModel()
-
-//        wordViewModel.allWords.observe(viewLifecycleOwner, { words ->
-//        Update the cached copy of the words in the adapter .
-//            words?.let { wordsAdapter.setWords(it) }
-//        })
-        moviesAdapter = context?.let { PopularMoviePagedListAdapter(it) }!!
-
+        apiService = ApiClient.getClient()
+        initRecycler()
         initSearchEditText()
         searchBtn.setOnClickListener {
             searchKey = movieNameEt.text.toString()
             if (searchKey.isNotEmpty()) {
-
+                // page =  1 here to reset it
+                searchMovies(searchKey, 1)
             } else {
                 Toast.makeText(context, getString(R.string.movie_empty), Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun searchMovies(searchKey: String, page: Int) {
+        compositeDisposable.add(
+            apiService.getSearchMovieAsync(searchKey, page)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        if (it.results.isNotEmpty()) {
+                            moviesList.addAll(it.results)
+                            moviesAdapter.notifyItemRangeInserted(
+                                moviesAdapter.itemCount,
+                                it.results.size
+                            )
+                        }
+                    },
+                    {
+                        it.message?.let { it1 -> Log.e("MovieDetailsDataSource", it1) }
+                    }
+                )
+        )
     }
 
     private fun initSearchEditText() {
@@ -91,11 +120,17 @@ class NormalSearchFragment : Fragment() {
     }
 
     private fun initRecycler() {
-        moviesAdapter = context?.let { PopularMoviePagedListAdapter(it) }!!
-        val linearLayoutManager = LinearLayoutManager(context)
+        moviesAdapter = context?.let { SearchMoverAdapter(it, moviesList) }!!
+        linearLayoutManager = LinearLayoutManager(context)
         moviesRv.layoutManager = linearLayoutManager
         moviesRv.setHasFixedSize(true)
         moviesRv.adapter = moviesAdapter
+
+        scroller = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                if (page > 1) searchMovies(searchKey, page)
+            }
+        }
     }
 
     private fun initWordRecycler() {
